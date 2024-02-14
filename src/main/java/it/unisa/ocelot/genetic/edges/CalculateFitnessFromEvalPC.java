de717bb0a3329ed53375c94cffc22bd7a5f175cf
elsonkurian1991 <e.kurian@campus.unimib.it>
2024-02-14 10:21:43 +0100

    add FType and Fitness cal java

---
+++ src/main/java/it/unisa/ocelot/genetic/edges/CalculateFitnessFromEvalPC.java
@@ -0,0 +1,99 @@
+package it.unisa.ocelot.genetic.edges;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class CalculateFitnessFromEvalPC {
+	public static Map<String, FType> filesWithFitnessVals = new HashMap<>();
+	public static boolean isFirsttime = false;
+
+	public static double CalculateFitness() {
+
+		double fitness = 0.0;
+		File directory = new File("./");// ocelot
+
+		// Check if the directory exists
+		if (directory.exists() && directory.isDirectory()) {
+			// Get all files in the directory
+			File[] files = directory.listFiles();
+			boolean alreadyFound =false;
+			for (FType set : filesWithFitnessVals.values()) {
+				if(!set.isTestGenerated())
+				alreadyFound= alreadyFound||set.isFirst();
+			}
+			// Iterate through the files and delete .epc files
+			if (files != null) {
+				for (File file : files) {
+					if (file.isFile() && file.getName().endsWith(".epc")) {
+						try {
+							double currFitness = Double
+									.parseDouble(new String(Files.readAllBytes(Paths.get(file.toURI()))));
+
+							System.out.println(file + " fitnessEvalPC is :" + currFitness);
+							if (filesWithFitnessVals.containsKey(file.toString())) {
+
+								FType tempFitness = filesWithFitnessVals.get(file.toString());
+								if (!tempFitness.isTestGenerated()) {
+
+									if (tempFitness.isTestCovered() && currFitness != 0) {
+										FType temp = new FType(Double.MAX_VALUE, true, false,tempFitness.isFirst());
+										filesWithFitnessVals.put(file.toString(), temp);
+									} else if (currFitness == 0 && tempFitness.isTestCovered()) {
+										FType temp = new FType(0, true, false,tempFitness.isFirst());							
+										filesWithFitnessVals.put(file.toString(), temp);
+									}
+									else if(currFitness==0){
+										FType temp = new FType(0, true, false,!alreadyFound);							
+										filesWithFitnessVals.put(file.toString(), temp);
+									}
+									else {
+										FType temp = new FType(currFitness, false, false,tempFitness.isFirst());
+										filesWithFitnessVals.put(file.toString(), temp);
+									}
+								}
+							} else {
+								if(currFitness==0) {
+									filesWithFitnessVals.put(file.toString(), new FType(0, true, false,true));
+								}
+								else {
+									filesWithFitnessVals.put(file.toString(), new FType(currFitness, false, false,false));
+								}
+								
+							}
+
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		} else {
+			System.out.println("Invalid directory path.");
+		}
+		System.out.println(filesWithFitnessVals);
+		for (Entry<String, FType> set : filesWithFitnessVals.entrySet()) {
+
+			System.out.println(set.getKey() + " = " + set.getValue());
+			if (!set.getValue().isTestGenerated()) {
+				if(set.getValue().isTestCovered()) {
+					return 0;
+				}
+				else {
+					fitness += set.getValue().getFitnessValue();
+				}
+		
+			}
+
+			// }
+		}
+		System.out.println("copy fitness." + fitness);
+		return fitness;
+	}
+
+}
---
+++ src/main/java/it/unisa/ocelot/genetic/edges/FType.java
@@ -0,0 +1,66 @@
+package it.unisa.ocelot.genetic.edges;
+
+import java.util.Objects;
+
+public class FType {
+	private double fitnessValue;
+	private boolean testCovered;
+	private boolean testGenerated;
+	private boolean first;
+	
+	public FType(double fitnessValue,boolean testCovered, boolean testGenerated, boolean first) {
+		// TODO Auto-generated constructor stub
+		this.fitnessValue =fitnessValue;
+		this.testCovered =testCovered;
+		this.testGenerated=testGenerated;
+		this.first= first;
+	}
+	public boolean isFirst() {
+		return first;
+	}
+	public void setFirst(boolean first) {
+		this.first = first;
+	}
+	public boolean isTestGenerated() {
+		return testGenerated;
+	}
+	public void setTestGenerated(boolean testGenerated) {
+		this.testGenerated = testGenerated;
+	}
+	
+	@Override
+	public String toString() {
+		return "FType [fitnessValue=" + fitnessValue + ", testCovered=" + testCovered + ", testGenerated="
+				+ testGenerated + ", first=" + first + "]";
+	}
+	public double getFitnessValue() {
+		return fitnessValue;
+	}
+	public void setFitnessValue(double fitnessValue) {
+		this.fitnessValue = fitnessValue;
+	}
+	public boolean isTestCovered() {
+		return testCovered;
+	}
+	public void setTestCovered(boolean firstTime) {
+		this.testCovered = firstTime;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, fitnessValue, testCovered, testGenerated);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FType other = (FType) obj;
+		return first == other.first
+				&& Double.doubleToLongBits(fitnessValue) == Double.doubleToLongBits(other.fitnessValue)
+				&& testCovered == other.testCovered && testGenerated == other.testGenerated;
+	}
+
+}
