14d4dfcfa40b3a75eb2b7e723c4d37cc72d41f1a
Luca14 <lucag14@gmail.com>
2024-07-03 16:28:45 +0200

    Improved management of max values for fitness values.

--- src/main/java/it/unisa/ocelot/genetic/edges/CalculateFitnessFromEvalPC3.java
+++ src/main/java/it/unisa/ocelot/genetic/edges/CalculateFitnessFromEvalPC3.java
@@ -19,225 +19,209 @@ import io.github.pavelicii.allpairs4j.Parameter;
 public class CalculateFitnessFromEvalPC3 {
 
 	public static boolean isFirsttime = false;
-    public static StringBuilder linesFromFitnessFiles=new StringBuilder();
+	public static StringBuilder linesFromFitnessFiles = new StringBuilder();
+
 	public static double CalculateFitness(Object[][][] arguments) {
 
 		double fitness = 0.0;
 
-		try (BufferedReader fiValFile = new BufferedReader(new FileReader("./fitnessValues.txt"))) { // TODO this file should written while parseing KQuery files.
+		try (BufferedReader fiValFile = new BufferedReader(new FileReader("./fitnessValues.txt"))) { // TODO this file
+																										// should
+																										// written while
+																										// parseing
+																										// KQuery files.
 			String lineBr;
-			ArrayList<String> keySetsToCheck=new ArrayList<>();
-			boolean alreadyFound =false;
+			ArrayList<String> keySetsToCheck = new ArrayList<>();
+			boolean alreadyFound = false;
 			while ((lineBr = fiValFile.readLine()) != null) {
-				//TODO read the fitness of each branch
-				String listOfItems[]=lineBr.split(";");
-				String fName=listOfItems[0];
-				String branchName=listOfItems[1];
-				String fitnessVal=listOfItems[2];
-				fitnessVal=fitnessVal.replaceAll(",", ".");
+				// TODO read the fitness of each branch
+				String listOfItems[] = lineBr.split(";");
+				String fName = listOfItems[0];
+				String branchName = listOfItems[1];
+				String fitnessVal = listOfItems[2];
+				fitnessVal = fitnessVal.replaceAll(",", ".");
 				double currFitness = Double.parseDouble(fitnessVal);
-				String fun_BranchName=fName+"_"+branchName;
-				for(Entry<String, FitType> entry:ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet()) {
-					//System.out.println(entry.getKey());
-					//System.out.println(fun_BranchName);
-					if(entry.getKey().contains(fun_BranchName)) {
-						//System.out.println(entry.getKey());
-						if(!checkIfKeySet(keySetsToCheck,fun_BranchName)) {
-							FitType tempFitness = entry.getValue(); 
-							alreadyFound= alreadyFound||tempFitness.isFirst();
+				String fun_BranchName = fName + "_" + branchName;
+				for (Entry<String, FitType> entry : ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals
+						.entrySet()) {
+					// System.out.println(entry.getKey());
+					// System.out.println(fun_BranchName);
+					if (entry.getKey().contains(fun_BranchName)) {
+						// System.out.println(entry.getKey());
+						if (!checkIfKeySet(keySetsToCheck, fun_BranchName)) {
+							FitType tempFitness = entry.getValue();
+							alreadyFound = alreadyFound || tempFitness.isFirst();
 
-							if(currFitness==0){
-								Fill_Files_PC_PairCom_FitnessVals(fun_BranchName,currFitness,true,false,!alreadyFound);
-							}
-							else {
+							if (currFitness == 0) {
+								Fill_Files_PC_PairCom_FitnessVals(fun_BranchName, currFitness, true, false,
+										!alreadyFound);
+							} else {
 
-								Fill_Files_PC_PairCom_FitnessVals(fun_BranchName,currFitness,false,false,tempFitness.isFirst());
+								Fill_Files_PC_PairCom_FitnessVals(fun_BranchName, currFitness, false, false,
+										tempFitness.isFirst());
 							}
 
 							keySetsToCheck.add(fun_BranchName);
 						}
 					}
 				}
-				linesFromFitnessFiles.append(lineBr+"\n");
+				linesFromFitnessFiles.append(lineBr + "\n");
 			}
 		} catch (IOException e) {
 			System.err.println("Error reading fitnessValues.txt file: " + e.getMessage());
 		}
-		Path pathTodel = Paths.get("./fitnessValues.txt");//delete the file every iteration
+		Path pathTodel = Paths.get("./fitnessValues.txt");// delete the file every iteration
 		try {
 			Files.delete(pathTodel);
-			//System.out.println("File fitnessValues.txt deleted successfully: ");
+			// System.out.println("File fitnessValues.txt deleted successfully: ");
 		} catch (IOException e) {
 			System.err.println("Error deleting file fitnessValues.txt: " + e.getMessage());
 		}
 		linesFromFitnessFiles.append("next iteration...\n");
-		//System.out.println(ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals);
-		
+		// System.out.println(ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals);
+
 		/*
-
-				//System.out.println(entry);
-
-
-		}
-
-		}
-
-		//List<Parameter> parameterList = new ArrayList<>();
-		//copy the list of combination to  filesWithFitnessVals
-		//boolean isFillPairCom =false;
-
-		//AllPairs listPairs=ReadEFLfilesforPairCombination.generateAllPairs(ReadEFLfilesforPairCombination.genParameterListEFL(parameterList));
-		//ReadEFLfilesforPairCombination.pathTCfitness.
-		//ReadEFLfilesforPairCombination.listofFunPaths.toString();
-		//ReadEFLfilesforPairCombination.generateAllPairs(null)
-		// Check if the directory exists
-		if (directory.exists() && directory.isDirectory()) {
-			// Get all files in the directory
-			File[] files = directory.listFiles();
-
-			// Iterate through the files and select only the .epc files  //ReadEFLfilesforPairCombination.pathTCfitness
-			// make it sort by type of files
-			if (files != null) {
-				for (File file : files) {
-					if (file.isFile() && file.getName().endsWith(".epc")) {
-						try {
-							double currFitness = Double
-									.parseDouble(new String(Files.readAllBytes(Paths.get(file.toURI()))));
-							String fileNameWOepc=extractFileNameOnly(file);
-							//System.out.println(fileNameWOepc);
-						//	System.out.println(file + " fitnessEvalPC is :" + currFitness);
-							ArrayList<String> keySetsToCheck=new ArrayList<>();
-							boolean alreadyFound =false;
-
-							for(Entry<String, FitType> entry:ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet()) {
-								if(entry.getKey().contains(fileNameWOepc)) {
-									//System.out.println(entry);
-						            if(!checkIfKeySet(keySetsToCheck,fileNameWOepc)) {
-									FitType tempFitness = entry.getValue(); 
-								//	if (!tempFitness.isTestGenerated()) {
-										alreadyFound= alreadyFound||tempFitness.isFirst();
-
-										if(currFitness==0){
-											Fill_Files_PC_PairCom_FitnessVals(fileNameWOepc,currFitness,true,false,!alreadyFound);
-										}
-										else {
-
-											Fill_Files_PC_PairCom_FitnessVals(fileNameWOepc,currFitness,false,false,tempFitness.isFirst());
-										}
-
-									keySetsToCheck.add(fileNameWOepc);
-								}
-
-							}
-
-							}
-
-
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		} else {
-			System.out.println("Invalid directory path.");
-		}
+		 * 
+		 * //System.out.println(entry);
+		 * 
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * //List<Parameter> parameterList = new ArrayList<>(); //copy the list of
+		 * combination to filesWithFitnessVals //boolean isFillPairCom =false;
+		 * 
+		 * //AllPairs listPairs=ReadEFLfilesforPairCombination.generateAllPairs(
+		 * ReadEFLfilesforPairCombination.genParameterListEFL(parameterList));
+		 * //ReadEFLfilesforPairCombination.pathTCfitness.
+		 * //ReadEFLfilesforPairCombination.listofFunPaths.toString();
+		 * //ReadEFLfilesforPairCombination.generateAllPairs(null) // Check if the
+		 * directory exists if (directory.exists() && directory.isDirectory()) { // Get
+		 * all files in the directory File[] files = directory.listFiles();
+		 * 
+		 * // Iterate through the files and select only the .epc files
+		 * //ReadEFLfilesforPairCombination.pathTCfitness // make it sort by type of
+		 * files if (files != null) { for (File file : files) { if (file.isFile() &&
+		 * file.getName().endsWith(".epc")) { try { double currFitness = Double
+		 * .parseDouble(new String(Files.readAllBytes(Paths.get(file.toURI())))); String
+		 * fileNameWOepc=extractFileNameOnly(file); //System.out.println(fileNameWOepc);
+		 * // System.out.println(file + " fitnessEvalPC is :" + currFitness);
+		 * ArrayList<String> keySetsToCheck=new ArrayList<>(); boolean alreadyFound
+		 * =false;
+		 * 
+		 * for(Entry<String, FitType>
+		 * entry:ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet())
+		 * { if(entry.getKey().contains(fileNameWOepc)) { //System.out.println(entry);
+		 * if(!checkIfKeySet(keySetsToCheck,fileNameWOepc)) { FitType tempFitness =
+		 * entry.getValue(); // if (!tempFitness.isTestGenerated()) { alreadyFound=
+		 * alreadyFound||tempFitness.isFirst();
+		 * 
+		 * if(currFitness==0){
+		 * Fill_Files_PC_PairCom_FitnessVals(fileNameWOepc,currFitness,true,false,!
+		 * alreadyFound); } else {
+		 * 
+		 * Fill_Files_PC_PairCom_FitnessVals(fileNameWOepc,currFitness,false,false,
+		 * tempFitness.isFirst()); }
+		 * 
+		 * keySetsToCheck.add(fileNameWOepc); }
+		 * 
+		 * }
+		 * 
+		 * }
+		 * 
+		 * 
+		 * } catch (IOException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); } } } } } else {
+		 * System.out.println("Invalid directory path."); }
 		 */
-		//till here.....................................
-		//System.out.println(filesWithFitnessVals);
-		boolean isCoveredThisTime=false;
-		for (Entry<String, FitType> set : ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet()) {			
+		// till here.....................................
+		// System.out.println(filesWithFitnessVals);
+		boolean isCoveredThisTime = false;
+		for (Entry<String, FitType> set : ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet()) {
 
 			if (!set.getValue().isTestGenerated()) {
-				FitType tempVal= set.getValue();
-				boolean testCovered=true;
-				for(FNameFitValType fnameVal: tempVal.getFname_Val()){
-					if (Math.abs(fnameVal.getFitnessVal()) > 0) {//to check is >= 0.0
-						//if(fnameVal.getFitnessVal().equals("0.0")){
-						testCovered=false;
-						fitness+= fnameVal.getFitnessVal();						
-					}					
+				FitType tempVal = set.getValue();
+				boolean testCovered = true;
+				for (FNameFitValType fnameVal : tempVal.getFname_Val()) {
+					if (Math.abs(fnameVal.getFitnessVal()) > 0) {// to check is >= 0.0
+						// if(fnameVal.getFitnessVal().equals("0.0")){
+						testCovered = false;
+						fitness = fitness + fnameVal.getFitnessVal();
+						if (fitness == Double.POSITIVE_INFINITY)
+							fitness = Double.MAX_VALUE;
+					}
 				}
-				if(testCovered) {
-					isCoveredThisTime=true;
+				if (testCovered) {
+					isCoveredThisTime = true;
 					tempVal.setTestCovered(testCovered);
-					String tempArgs= tempVal.getArgumentList();
-					tempArgs=tempArgs.replaceAll("null", "");
-					String args=GetArguInString(arguments);
-					if(!tempArgs.contains(args)) {
-						tempArgs=tempArgs+args;
+					String tempArgs = tempVal.getArgumentList();
+					tempArgs = tempArgs.replaceAll("null", "");
+					String args = GetArguInString(arguments);
+					if (!tempArgs.contains(args)) {
+						tempArgs = tempArgs + args;
 					}
 					tempVal.setArgumentList(tempArgs);
-				}
-				else {
+				} else {
 					tempVal.setTestCovered(testCovered);
 				}
 
 			}
-			/*if(set.getValue().isTestCovered()) {
-					System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-					ReadEFLfilesforPairCombination.CheckTCisCovered(ReadEFLfilesforPairCombination.pathTCfitness,arguments);//to update the combinations
-					return 0;
-				}
-				else {
-					fitness +=10; //set.getValue().getFitnessValue();//TODO
-				}
+			/*
+			 * if(set.getValue().isTestCovered()) {
+			 * System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+			 * ); ReadEFLfilesforPairCombination.CheckTCisCovered(
+			 * ReadEFLfilesforPairCombination.pathTCfitness,arguments);//to update the
+			 * combinations return 0; } else { fitness +=10;
+			 * //set.getValue().getFitnessValue();//TODO }
 			 */
-
-
 
 			// }
 		}
-		//ReadEFLfilesforPairCombination.CheckTCisCovered(ReadEFLfilesforPairCombination.pathTCfitness);//to update the combinations
+		// ReadEFLfilesforPairCombination.CheckTCisCovered(ReadEFLfilesforPairCombination.pathTCfitness);//to
+		// update the combinations
 
-		if(isCoveredThisTime) {
+		if (isCoveredThisTime) {
 			return 0;
-		}
-		else {
-			//System.out.println("copy fitness." + fitness);
+		} else {
+			// System.out.println("copy fitness." + fitness);
 			return fitness;
 		}
 
 	}
 
-
-
 	public static String GetArguInString(Object[][][] args2) {
-		String argsList="";
+		String argsList = "";
 		for (int i = 0; i < args2.length; i++) {
 			for (int j = 0; j < args2[i].length; j++) {
 				for (int k = 0; k < args2[i][j].length; k++) {
-					//  System.out.println("args2[" + i + "][" + j + "][" + k + "] = " + args2[i][j][k]);
-					argsList=argsList+args2[i][j][k].toString();
-					argsList=argsList+",";
+					// System.out.println("args2[" + i + "][" + j + "][" + k + "] = " +
+					// args2[i][j][k]);
+					argsList = argsList + args2[i][j][k].toString();
+					argsList = argsList + ",";
 				}
 
 			}
 
 		}
-		argsList=argsList.substring(0, argsList.length()-1);
-		argsList=argsList+";";
+		argsList = argsList.substring(0, argsList.length() - 1);
+		argsList = argsList + ";";
 
-		//System.out.println(argsList);
+		// System.out.println(argsList);
 		return argsList;
 	}
 
-
-
 	private static boolean checkIfKeySet(ArrayList<String> keySetsToCheck, String fileNameWOepc) {
 		// TODO Auto-generated method stub
-		boolean isfound=false;
-		if(keySetsToCheck.isEmpty()) {
-			isfound= false;
-		}
-		else {
+		boolean isfound = false;
+		if (keySetsToCheck.isEmpty()) {
+			isfound = false;
+		} else {
 			for (String key : keySetsToCheck) {
-				if(key.contentEquals(fileNameWOepc)) {
-					isfound= true;
-				}
-				else {
-					isfound= false;
+				if (key.contentEquals(fileNameWOepc)) {
+					isfound = true;
+				} else {
+					isfound = false;
 				}
 
 			}
@@ -246,54 +230,47 @@ public class CalculateFitnessFromEvalPC3 {
 		return isfound;
 	}
 
-
-
 	private static void Fill_Files_PC_PairCom_FitnessVals(String fileName, double currFitness, boolean testCovered,
-			boolean testGenerated,boolean first) {
+			boolean testGenerated, boolean first) {
 		// TODO Auto-generated method stub
-		//FitType tempFitType= new FitType(testCovered, testGenerated, first, "", false);
-		//List<FNameFitValType> Fname_Vals= new ArrayList<FNameFitValType>();
-		for(Entry<String, FitType> entry:ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet()) {
-			if(entry.getKey().contains(fileName)) {
-				//if(!entry.getValue().isTestCovered()) {
-				for(FNameFitValType fnameVal: entry.getValue().getFname_Val()){
-					if(fnameVal.getfName().toString().contentEquals(fileName)) {
-						//System.out.println(fnameVal.getfName()+"°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+		// FitType tempFitType= new FitType(testCovered, testGenerated, first, "",
+		// false);
+		// List<FNameFitValType> Fname_Vals= new ArrayList<FNameFitValType>();
+		for (Entry<String, FitType> entry : ReadEFLfilesforPairCombination.files_PC_PairCom_FitnessVals.entrySet()) {
+			if (entry.getKey().contains(fileName)) {
+				// if(!entry.getValue().isTestCovered()) {
+				for (FNameFitValType fnameVal : entry.getValue().getFname_Val()) {
+					if (fnameVal.getfName().toString().contentEquals(fileName)) {
+						// System.out.println(fnameVal.getfName()+"°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
 						fnameVal.setFitnessVal(currFitness);
-						//Fname_Vals.add(fnameVal);
+						// Fname_Vals.add(fnameVal);
 					}
 				}
 				entry.getValue().setFirst(first);
-				//}
-				/*else {
-					if(currFitness>0.0) {
-						for(FNameFitValType fnameVal: entry.getValue().getFname_Val()){
-							if(fnameVal.getfName().toString().contentEquals(fileName)) {
-								System.out.println(fnameVal.getfName()+"°°°°°°°already test convered°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-								fnameVal.setFitnessVal(Double.MAX_VALUE);
-								//Fname_Vals.add(fnameVal);
-							}
-						}
-					}
-				}*/
+				// }
+				/*
+				 * else { if(currFitness>0.0) { for(FNameFitValType fnameVal:
+				 * entry.getValue().getFname_Val()){
+				 * if(fnameVal.getfName().toString().contentEquals(fileName)) {
+				 * System.out.println(fnameVal.getfName()
+				 * +"°°°°°°°already test convered°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+				 * fnameVal.setFitnessVal(Double.MAX_VALUE); //Fname_Vals.add(fnameVal); } } } }
+				 */
 			}
 
 		}
-		//tempFitType.setFname_Val(Fname_Vals);
-		//entry.setValue(tempFitType);
+		// tempFitType.setFname_Val(Fname_Vals);
+		// entry.setValue(tempFitType);
 	}
 
-
-
-
 	private static void FillPairWiseList(String fileNameWOepc, double currFitness) {
-		//ReadEFLfilesforPairCombination.pathTCfitness
-		for(Entry<String, EFLType> entry:ReadEFLfilesforPairCombination.pathTCfitness.entrySet()) {
-			if(!entry.getValue().isTCcovered()) { //update the new fitness only is not covered
-				for(FNameFitValType fnameVal: entry.getValue().getFname_Val()){
-					//System.out.println(fnameVal.getfName()+"-"+fileNameWOepc);
-					if(fnameVal.getfName().toString().contentEquals(fileNameWOepc)) {
-						//	System.out.println(fnameVal.getfName()+"°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+		// ReadEFLfilesforPairCombination.pathTCfitness
+		for (Entry<String, EFLType> entry : ReadEFLfilesforPairCombination.pathTCfitness.entrySet()) {
+			if (!entry.getValue().isTCcovered()) { // update the new fitness only is not covered
+				for (FNameFitValType fnameVal : entry.getValue().getFname_Val()) {
+					// System.out.println(fnameVal.getfName()+"-"+fileNameWOepc);
+					if (fnameVal.getfName().toString().contentEquals(fileNameWOepc)) {
+						// System.out.println(fnameVal.getfName()+"°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
 						fnameVal.setFitnessVal(currFitness);
 					}
 				}
@@ -302,13 +279,13 @@ public class CalculateFitnessFromEvalPC3 {
 	}
 
 	private static String extractFileNameOnly(File file) {
-		String fName=file.toString();
+		String fName = file.toString();
 		if (fName == null || fName.isEmpty()) {
 			return "";
 		}
 		int indexOfSeparator = Math.max(fName.lastIndexOf('/'), fName.lastIndexOf('\\'));
-		fName= fName.substring(indexOfSeparator + 1);
-		return fName=fName.replaceAll(".epc", "");
+		fName = fName.substring(indexOfSeparator + 1);
+		return fName = fName.replaceAll(".epc", "");
 	}
 
 }
