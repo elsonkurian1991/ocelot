148effd375c4417fe27f59f31b2082ce294a392f
Luca Guglielmo <l.guglielmo@campus.unimib.it>
2025-05-07 17:50:45 +0200

    Switched to List from Set.

--- src/main/java/it/unisa/ocelot/simulator/GenericCoverageCalculator.java
+++ src/main/java/it/unisa/ocelot/simulator/GenericCoverageCalculator.java
@@ -1,30 +1,24 @@
 package it.unisa.ocelot.simulator;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import it.unisa.ocelot.TestCase;
 import it.unisa.ocelot.c.cfg.CFG;
-import it.unisa.ocelot.c.cfg.edges.FlowEdge;
-import it.unisa.ocelot.c.cfg.edges.LabeledEdge;
-import it.unisa.ocelot.c.cfg.nodes.CFGNode;
 import it.unisa.ocelot.genetic.objectives.BranchDistanceCache;
 import it.unisa.ocelot.genetic.objectives.GenericObjective;
-import it.unisa.ocelot.simulator.listeners.CoverageCalculatorListener;
 import it.unisa.ocelot.util.Utils;
 
 public class GenericCoverageCalculator {
 	private CFG cfg;
-	private Set<GenericObjective> objectives;
-	private Set<GenericObjective> coveredObjectives = new HashSet<GenericObjective>();
+	private List<GenericObjective> objectives;
+	private List<GenericObjective> coveredObjectives = new ArrayList<GenericObjective>();
 	private double objectiveCoverage = 0;
 
 	public GenericCoverageCalculator(CFG cfg, List<GenericObjective> objectives) {
 		this.cfg = cfg;
-		this.objectives = new HashSet<GenericObjective>();
-		this.objectives.addAll(objectives);
+		this.objectives = objectives;
 	}
 
 	public void calculateCoverage(List<Object[][][]> pParametersList) {
@@ -70,12 +64,12 @@ public class GenericCoverageCalculator {
 		return this.objectiveCoverage;
 	}
 
-	public Set<GenericObjective> getCoveredObjectives() {
+	public List<GenericObjective> getCoveredObjectives() {
 		return this.coveredObjectives;
 	}
 	
-	public Set<GenericObjective> getUncoveredObjectives() {
-		Set<GenericObjective> uncovered = new HashSet<GenericObjective>(this.objectives);
+	public List<GenericObjective> getUncoveredObjectives() {
+		List<GenericObjective> uncovered = new ArrayList<GenericObjective>(this.objectives);
 		uncovered.removeAll(this.coveredObjectives);
 		return uncovered;
 	}
